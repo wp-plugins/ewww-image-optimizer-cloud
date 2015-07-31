c6a4efe9432a5405c377216660d1716a47040fc9
nosilver4u <nosilver4u@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-31 23:12:24 +0000

    tagging and releasing 2.5
    git-svn-id: https://plugins.svn.wordpress.org/ewww-image-optimizer-cloud/trunk@1210627 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ewww-image-optimizer-cloud.php
+++ ewww-image-optimizer-cloud.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Integrate cloud image optimization into WordPress.
- * @version 2.4.7.6
+ * @version 2.5.0
  * @package EWWW_Image_Optimizer_Cloud
  */
 /*
@@ -10,7 +10,7 @@ Plugin URI: http://ewww.io/
 Description: Reduce file sizes for images within WordPress including NextGEN Gallery and GRAND FlAGallery via paid cloud service.
 Author: Shane Bishop
 Text Domain: ewww-image-optimizer-cloud
-Version: 2.4.7.6
+Version: 2.5.0
 Author URI: https://ewww.io/
 License: GPLv3
 */
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: nosilver4u
 Tags: images, image, attachments, attachment, optimize, optimization, lossless, photos, photo, picture, pictures, seo, compression, image editor, gmagick, cloud, wp-cli
 Requires at least: 3.5
 Tested up to: 4.3
-Stable tag: 2.4.7
+Stable tag: 2.5.0
 License: GPLv3
 
 Reduce file sizes for images within WordPress including NextGEN, GRAND FlAGallery and more via paid cloud service.
